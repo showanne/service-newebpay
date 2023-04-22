8a5fef4683e794ff1240cf270b06db3ac9e530ae
Anne <anne@ks-design.com.tw>
2023-04-22 13:32:29 +0800

    Init

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '藍新金流付款' });
+});
+
+
+module.exports = router;
