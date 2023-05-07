33c91b9e9af2747da642908ceae3a620ae884300
Anne <anne@ks-design.com.tw>
2023-05-08 01:34:35 +0800

    Add：DB schema & setting

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+dotenv.config({path:".env"}); // 使用環境變數 process.env.Name
+
+// 將 DB 資料及密碼替換成環境參數
+const DB = process.env.Database.replace(
+    '<password>', process.env.Database_pwd
+)
+
+mongoose
+.connect(DB)
+.then(() => console.log('資料庫連接成功'));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
---
+++ model/orderModels.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const orderSchema = new mongoose.Schema({
+  user_name: {
+    type: String,
+    required: [true, "個人名稱未填寫"],
+  },
+  user_email: {
+    type: String,
+    required: [true, "電子郵件未填寫"],
+    unique: true,
+    lowercase: true,
+    // 自訂驗證，安裝套件 npm i validator
+    validate: {
+      validator() {
+        return validator.isEmail(value);
+      },
+      message: "信箱格式不正確",
+    },
+  },
+  project: {
+    type: String,
+    required: [true, "專案商品未填寫"],
+  },
+  order_note: { // 訂單備註
+    type: String,
+    trim: true
+  },
+  order_create_date: { // 訂單建立日期
+    type: Date,
+    default: Date.now()
+  },
+  order_status: { // 訂單狀態
+    type: Number,
+    default: 0 // 預設 0-未完成 (0-未完成 / 1-處理中 / 2-已完成 ... )
+  },
+  order_final_date: { // 訂單完成日期 <- 預設金流回傳付款完成就算訂單完成
+    type: Date
+  },
+  payment_price: {
+    // 關聯訂單總金額
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  payment_method: {
+    // 付款方式
+    type: String,
+    enum: ["WebATM", "ATM轉帳", "條碼繳費",  "超商代碼繳費", "信用卡"],
+    required: true,
+  },
+  payment_status: {
+    // 付款狀態
+    type: Number,
+    default: 0, // 預設 0-未付款 (0-未付款 / 1-待付款 / 2-付款完成 / 3-付款失敗 / 4-取消交易 ... )
+  },
+  newebpay_tokens: {
+    type: [
+      {
+        aes_encrypt: String,
+        sha_encrypt: String
+      },
+    ],
+  }
+});
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
